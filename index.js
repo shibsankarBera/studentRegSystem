5fb27935d11967f0b71e0fa6ea77c0acb6c7ac40
shibsankarBera <shibu900099@gmail.com>
2025-12-31 21:49:46 +0530

    implementation of CURD operation & save localstorage are succesfull

--- index.js
+++ index.js
@@ -7,32 +7,42 @@ let stContact = document.getElementById("stContact");
 var stname = document.getElementById("stname");
 var submibutton = document.getElementById("submitbutton");
 
-const students = [];
-console.log(students);
+// showing record on page load
+let students = [];
+if (localStorage.getItem("students")) {
+  students = JSON.parse(localStorage.getItem("students"));
+}
+displayStudents();
 
 ////submit event handler
-
+ let editIndex=-1;
 submibutton.addEventListener("click", () => {
-  console.log("not submitted");
 
   //extract values
   let Name = stname.value.trim(),
-    ID = stID.value.trim(""),
-    Email = stEmail.value.trim(""),
-    Contact = stContact.value.trim("");
+    ID = stID.value.trim(),
+    Email = stEmail.value.trim(),
+    Contact = stContact.value.trim();
 
   //create object to store valid inputs
   let student = { Name, ID, Email, Contact };
   // Validate form
   let isvalidform = validateInputs(Name, ID, Email, Contact);
   if (isvalidform) {
-    console.log("if valid", isvalidform);
-    students.push(student);
-    console.log("if part",students)
-  } else {
-    console.log("if not valid", isvalidform);
-    console.log("ele part",students)
+     if(editIndex==-1){
+      students.push(student);//add record
+     alert("record added sucessfull");
+     clearForm();
+    }else{///edit exixting record
+      students[editIndex]=student;
+    alert("record Edit sucessfull");
+    editIndex=-1;
+    clearForm();
+    }  
   }
+saveToLocalStorage();
+  displayStudents();
+
 });
 
 // ===================================form validation start========================
@@ -141,6 +151,8 @@ function isValidateContact(contactInput, contact) {
   return true;
 }
 
+// ===================================Helper function ========================
+
 //--------- set success & eror mesagess------helper function
 
 let setSucess = function (input) {
@@ -157,4 +169,88 @@ let setError = function (input, message) {
   let small = inputcontrol.querySelector(".small");
   small.innerHTML = message;
 };
+
+
+// -----------------clear input form------------
+function clearForm(){
+  stID.value= "";
+  stEmail.value ="";
+  stContact.value = "";
+   stname.value ="";
+}
 // ===================================form validation END ========================
+
+// // ===================================Display section========================
+
+function displayStudents(){
+ tBody = document.getElementById("display-items");
+ tBody.innerHTML="";
+  for (i = 0; i < students.length; i++) {
+    const row = document.createElement("tr");//create row
+    // row.classList.add("input-row");
+    row.innerHTML = ` 
+                <td>${i+1}</td>
+                <td>${students[i].Name}</td>
+                <td>${students[i].ID}</td>
+                <td>${students[i].Email}</td>
+                <td>${students[i].Contact}</td>
+                <td>
+                  <button onclick="deleteStudent(${i})" class="delete">
+                    <i class="ri-delete-bin-line"></i>
+                  </button>
+                </td>
+                <td>
+                  <button onclick="editStudent(${i})" class="edit"><i class="ri-edit-2-line"></i></button>
+                </td>
+              
+              `
+              tBody.appendChild(row);
+
+  }
+ 
+  
+}
+
+//displayStudents();
+ 
+// --------------------delete student details-----------
+function deleteStudent(index){
+   students.splice(index, 1);
+    saveToLocalStorage();
+    displayStudents()
+}
+// --------------------Edit student details-----------
+function editStudent(index){
+  editIndex=index;
+  console.log("edit button clicked")
+  let student=students[index];
+  stID.value= student.ID;
+  stEmail.value =student.Email;
+  stContact.value = student.Contact;   stname.value =student.Name;
+  
+}
+
+// =============================Dynamic ScrollBar================
+  const cont =document.getElementById("box");
+  const tabl=document.getElementById("tabl");
+  
+  function dynamicScroll(){
+    if (cont.offsetHeight<tabl.offsetHeight){
+       cont.style.overflowY="scroll";
+    }else{
+      cont.style.overflowY="hidden";
+    }
+    
+  }  
+   dynamicScroll();
+  
+  
+
+
+  // =============================save local storage================
+    function saveToLocalStorage() {
+  localStorage.setItem("students", JSON.stringify(students));
+  
+}
+
+ 
